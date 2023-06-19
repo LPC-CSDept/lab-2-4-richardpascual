77168da8c7083a27c411679a64379b6b7359c410
Richard Pascual <richardpascual>
2023-06-18 22:40:21 -0700

    Week 2, Lab 4

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     # Comlete your code here
     ##################################################
 
+    sub1 = original_str[:6]
+    sub2 = original_str[-11:]
+    merged_str = sub2 + ' ' + sub1
+
     print(sub2)
     print(sub1)
     print(merged_str)
